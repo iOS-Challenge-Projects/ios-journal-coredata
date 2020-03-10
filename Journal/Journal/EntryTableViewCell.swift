97deef01b94a3d9a657a65108440bdd3e94ae2b4
fritzgt <tuanis@live.com>
2020-03-10 05:23:22 +0200

    create views

---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by FGT MAC on 3/10/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
